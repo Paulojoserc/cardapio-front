f727381df62b881298df615032e1fb108fcc1e7e
Paulojoserc <pjrc83@gmail.com>
2024-01-30 20:35:49 -0300

    resovendo problema do card.tsx

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,8 +1,17 @@
+import "./card.css";
 interface CardProps{
-
+price: number,
+title: string,
+image: string
 }
-export function Card(props : CardProps){
+
+export function Card({ price, image, title } : CardProps){
     return(
-        <></>
+        <div className="cade">
+            <img src={image} />
+            <h2>{title}</h2>
+            <p><b>Valor</b>{price}</p>
+        </div>
     )
 }
+
