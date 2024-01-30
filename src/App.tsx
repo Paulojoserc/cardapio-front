0bba396ea1d4027a3d731806916a9aa77416ca48
Paulojoserc <pjrc83@gmail.com>
2024-01-30 18:36:54 -0300

    card

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <h1>Cardápio</h1>
+        <div className="card-grid">
+          
+        </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
